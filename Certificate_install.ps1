94b6e8bbd242a38c1778f7636527c557abfbd7db
korinekzdenek <52947887+korinekzdenek@users.noreply.github.com>
2019-07-16 09:43:42 +0200

    Fix

--- Certificate_install.ps1
+++ Certificate_install.ps1
@@ -2,4 +2,4 @@
 $env:COMPUTERNAME
 If (!(Test-Path Z:)){New-PSDrive -Name Z -PSProvider FileSystem -Root "\\czstrts01\SHP_16_DATA"}
 $pws = convertto-securestring "mainstream" -asplaintext -force
-Import-PfxCertificate -FilePath Z:\Install\cert\wildcard.cpi.cz-2018.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $pws
+Import-PfxCertificate -FilePath Z:\Install\cert\wildcard.cpi.cz-2018.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $pw
